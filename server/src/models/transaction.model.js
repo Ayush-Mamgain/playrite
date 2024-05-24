ca2e4b18a18e370518343950a95989a3f2b16cb0
Ayush-Mamgain <ayushmamgain1234@gmail.com>
2024-05-24 11:00:13 +0530

    Design transaction model

---
+++ server/src/models/transaction.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+    type: {
+        type: String,
+        required: true,
+        enum: ['deposit', 'withdrawal']
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 100
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed', 'failed'],
+        required: true,
+        default: 'pending'
+    },
+}, { timestamps: true });
+
+const Transaction = mongoose.model('Transaction', transactionSchema);
+module.exports = Transaction;
