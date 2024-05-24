7e52a93f8a7d9d939cb5e2488760f4f77c9ea504
Ayush-Mamgain <ayushmamgain1234@gmail.com>
2024-05-24 12:19:17 +0530

    fix bug

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -20,7 +20,8 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     bank: {
-        type: mongoose.Schema.Types.ObjectId
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Bank'
     },
     role: {
         type: String,
