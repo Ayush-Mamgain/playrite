254e7da9010a72e2343f93080d92a8c7c84e0f03
Ayush-Mamgain <ayushmamgain1234@gmail.com>
2024-05-25 15:33:20 +0530

    DB connection successful, server started successfully

---
+++ server/src/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+
+//create an instance of express server
+const app = express();
+
+//Just mount the middlewares here
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({
+    limit: '16kb'
+}));
+
+app.use(express.urlencoded({
+    limit: '16kb',
+    extended: true
+}));
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+//setup default route
+app.get('/', (req, res) => res.send('Welcome to charity finder'));
+
+//mount API routes on the app
+
+module.exports = app;
